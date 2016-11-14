978f3897dd354a5e5b83abe1e62d35ed7e87840e
Avi Cavale <avi@shippable.com>
2016-11-15 00:52:10 +0900

    updating tests

--- test.js
+++ test.js
@@ -8,13 +8,30 @@ var chai = require('chai');
 var expect = chai.expect;
 var should = require('should');
 
-describe('Index', function () {
-  it('Testing Index call to ' + apiURL, function (done) {
-    superagent.get(apiURL)
-      .end(function (err, res) {
-        (err === null).should.equal(true);
-        res.statusCode.should.equal(200);
-        done();
-      });
-  });
-});
+describe('Core Test Suite',
+  function () {
+    it('Testing Index call to ' + apiURL,
+      function (done) {
+        superagent.get(apiURL).end(
+          function (err, res) {
+            (err === null).should.equal(true);
+            res.statusCode.should.equal(200);
+            done();
+          }
+        );
+      }
+    );
+    it('Testing Info call to ' + apiURL,
+      function (done) {
+        superagent.get(apiURL + 'info').end(
+          function (err, res) {
+            (err === null).should.equal(true);
+            res.statusCode.should.equal(200);
+            res.body.time.should.equal(true);
+            done();
+          }
+        );
+      }
+    );
+  }
+);
